4967e653243fa6a13dc833c154c1cc2fba20a23f
componavt <andrew.krizhanovsky@gmail.com>
2026-06-26 14:38:00 +0300

    start game

---
+++ app.py
@@ -0,0 +1,12 @@
+import gradio as gr
+
+def greet(name, intensity):
+    return "Hello, " + name + "!" * int(intensity)
+
+demo = gr.Interface(
+    fn=greet,
+    inputs=["text", gr.Slider(value=1, minimum=1, maximum=10, step=1)],
+    outputs="text",
+)
+
+demo.launch()
