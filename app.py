de4e8bc693f6cf0f6ae539e1ba7d8c81ab566b61
componavt <andrew.krizhanovsky@gmail.com>
2026-06-29 14:30:38 +0300

    demo.launch back

--- app.py
+++ app.py
@@ -8,3 +8,6 @@ demo = gr.Interface(
     inputs=["text", gr.Slider(value=1, minimum=1, maximum=10, step=1)],
     outputs="text",
 )
+
+if __name__ == "__main__":
+    demo.launch(server_name="0.0.0.0", server_port=7860)
